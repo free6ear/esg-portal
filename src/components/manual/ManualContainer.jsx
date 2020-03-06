415a2efa3c5706475ee64ccde21c6de6c521dabe
free6ear <free6ear@gmail.com>
2020-03-06 17:36:51 +0800

    before eject

--- src/components/manual/ManualContainer.jsx
+++ src/components/manual/ManualContainer.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Anchor } from 'antd';
+import { Anchor, Row, Col, Layout, Menu } from 'antd';
 import ReactMarkdown from 'react-markdown';
-import MarkNav from 'markdown-navbar';
+import MarkdownNavbar from 'markdown-navbar';
 import 'markdown-navbar/dist/navbar.css';
-
 import HaierManual from '../../md/haier_manual.md'
+
+const { Header, Content, Sider } = Layout;
 
 export default class ManualContainer extends Component {
     constructor(props) {
@@ -21,21 +22,30 @@ export default class ManualContainer extends Component {
     }
 
     render() {
-        const {markdown} = this.state;
-        return (
-            <div>
-                <Anchor>
-                    <MarkNav
-                      source={markdown}
-                    />
-                </Anchor>
+    const {markdown} = this.state;
+    return (
+        <Content style={{ padding: '0 50px' }}>
+        <Layout className="site-layout-background" style={{ padding: '24px 0' }}>
+            <Sider className="site-layout-background" width={200}>
+                <Menu
+                    mode="inline"
+                    style={{ height: '100%' }}
+                >
+                <div className="navigation">
+                    <MarkdownNavbar source={markdown} />
+                </div>
+                </Menu>
+            </Sider>
+            <Content style={{ padding: '0 24px', minHeight: 280 }}>                    
                 <ReactMarkdown 
-                  source={markdown}
-                  escapeHtml={false}
-                  className="markdown-body"
+                    source={markdown}
+                    escapeHtml={false}
+                    className="markdown-body"
                 />
-            </div>
-        )
+            </Content>
+        </Layout>
+    </Content>
+    )
     }
 }
 
